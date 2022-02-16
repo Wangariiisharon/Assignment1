e2afc64fb2342ed47a661bf17104c791f300c3c2
Wangariiisharon <mwangiiisharon@gmail.com>
2022-02-16 22:14:03 +0300

    first assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var name:String="Mary Njeri"
+    println(name)
+    var number: String="0700620110"
+    println(number)
+    var age:Int=12
+    println(age)
+    var weight:Int=60
+    println(weight)
+    var kenyan:Boolean=true
+    println(kenyan)
+}
